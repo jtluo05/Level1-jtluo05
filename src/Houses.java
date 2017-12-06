699d6eaf8d2c25a3efa9bf5300c7aba32c4938e7
TJHL <league@iMac.attlocal.net>
2017-12-05 19:54:25 -0800

    houses ip

--- src/Houses.java
+++ src/Houses.java
@@ -6,8 +6,9 @@ import org.jointheleague.graphical.robot.Robot;
 public class Houses {
 	static Robot bob = new Robot("mini");
 	static Random num = new Random();
-
-	public static void main(String[] args) {
+static String height;
+static String color;
+public static void main(String[] args) {
 
 		bob.setSpeed(100);
 		bob.turn(180);
@@ -16,48 +17,89 @@ public class Houses {
 		bob.move(275);
 		bob.setAngle(0);
 		for (int i = 0; i < 10; i++) {
-			int h = num.nextInt(2);
-if (h==0) {
-	height="small";
-}
-			house(h + 100);
+			int h = num.nextInt(3);
+			if (h == 0) {
+				 height = "small";
+			}
+			if (h == 1) {
+				 height = "medium";
+			}
+			else if (h == 2) {
+				 height = "large";
+			}
+		
+			int c = num.nextInt(3);
+			 if (c == 1) {
+				 color = "green";
+			}
+			else if (c == 2) {
+				 color = "blue";
+			}
+			else if (c == 0) {
+				 color = "red";
+			}
+			else {
+				color= "green";
+			}
+			house(height,color);
 
 		}
 	}
 
-	static void house(String height) {
-	int sml=500;
+	static void house(String height, String color) {
+		int sml = 500;
+		
 		if (height.equals("small")) {
-			 sml = 60;
+			sml = 60;
 		}
-		if (height.equals("meduim")) {
-			 sml = 120;
+		else if (height.equals("medium")) {
+			sml = 120;
 		}
-		if (height.equals("large")) {
-			 sml = 250;
-		}
+		else if (height.equals("large")) {
+			sml = 250;
+	}
+		
 		bob.setAngle(0);
+		bob.setPenWidth(10);
 		bob.penDown();
-		bob.setPenColor(Color.green);
+	bob.setWindowColor(Color.black);
+	if (color.equals("blue")) {
+		bob.setPenColor(Color.blue);
+	}
+	else if (color.equals("green")) {
+		bob.setPenColor(Color.WHITE);
+	}
+	else 	if (color.equals("red")) {
+		bob.setPenColor(Color.red);
+	}
 		bob.move(sml);
 		bob.turn(90);
 		bob.move(50);
 		bob.turn(90);
 		bob.move(sml);
 		bob.turn(270);
-		bob.move(30);
+	bob.setPenColor(Color.green);	
+	bob.move(30);
 
-
-
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
 	}
+void drawPointyRoof() {
+bob.turn(45);	
+	bob.move(35);
+	bob.turn(90);
+	bob.move(35);
+	bob.turn(45);
+}
+void drawFlatRoof() {
+	bob.turn(90);
+bob.move(50);
+bob.turn(90);
+}
+
+
+
+
+
+
+
+
 }
