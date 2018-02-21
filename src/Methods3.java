e39a48e4fa0155fd2e262ef4c9fdcc27e14987cb
jtluo05 <league@iMac-2.attlocal.net>
2018-02-20 19:56:36 -0800

    Module3

---
+++ src/Methods3.java
@@ -0,0 +1,31 @@
+
+public class Methods3 {
+public static void main(String[] args) {
+	eat("Burgers");
+double i= getDinnerTime();
+String name=getFavoriteFood("Jordan");
+
+
+
+
+
+
+
+}
+static void eat(String food) {System.out.println("hi");}
+static double getDinnerTime() {return 0;}
+static String getFavoriteFood(String personsName) {
+return personsName;}
+
+
+
+
+
+
+
+
+
+
+
+
+}
